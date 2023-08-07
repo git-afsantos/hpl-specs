3b68250d06c1677f943a05aaa1bdd2ab2b8f9e42
André Santos <git-afsantos@users.noreply.github.com>
2023-08-07 16:49:21 +0100

    new package structure

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## v1.0.0 - 2023-08-07
+### Changed
+- Modernized tooling and package structure.
+
 ## v0.2.3 - 2021-08-27
 ### Fixed
 - Fixed a bug with properties that had complex events such as disjunctions.
--- src/hpl/__init__.py
+++ src/hpl/__init__.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
-
 # SPDX-License-Identifier: MIT
 # Copyright © 2021 André Santos
 
-__version__ = "0.2.3"
+###############################################################################
+# Imports
+###############################################################################
+
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+
+###############################################################################
+# Constants
+###############################################################################
+
+try:
+    __version__ = version('hpl')
+except PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    __version__ = 'unknown'
