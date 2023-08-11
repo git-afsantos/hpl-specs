437d3217d8520e3eea7615db23870fdbf671a634
André Santos <git-afsantos@users.noreply.github.com>
2023-08-11 11:40:26 +0100

    Update _unused.py

--- src/hpl/ast/_unused.py
+++ src/hpl/ast/_unused.py
@@ -211,3 +211,212 @@ class HplPredicate(HplAstObject):
             elif t.is_string:
                 accessor._type_check(expr, T_STR)
             expr.ros_type = t
+
+
+
+###############################################################################
+# Type Tokens
+###############################################################################
+
+
+@frozen
+class TypeToken:
+    name: str
+
+    def __attrs_pre_init__(self):
+        raise TypeError('cannot instantiate an abstract class')
+
+    @property
+    def type(self) -> DataType:
+        raise NotImplementedError()
+
+    @property
+    def is_bool(self) -> bool:
+        return self.type.can_be_bool
+
+    @property
+    def is_number(self) -> bool:
+        return self.type.can_be_number
+
+    @property
+    def is_string(self) -> bool:
+        return self.type.can_be_string
+
+    @property
+    def is_message(self) -> bool:
+        return self.type.can_be_message
+
+    @property
+    def is_array(self) -> bool:
+        return self.type.can_be_array
+
+    @property
+    def is_range(self) -> bool:
+        return self.type.can_be_range
+
+    @property
+    def is_set(self) -> bool:
+        return self.type.can_be_set
+
+
+@frozen
+class StringType(TypeToken):
+    @property
+    def type(self) -> DataType:
+        return DataType.STRING
+
+
+@frozen
+class EnumeratedType(TypeToken):
+    _type: DataType = field(validator=in_(PRIMITIVE_TYPES))
+    values: Tuple[Any] = field(factory=tuple, converter=tuple, validator=instance_of(tuple))
+
+    @values.validator
+    def _check_values(self, _attribute, values: Tuple[Any]):
+        if self._type is DataType.BOOL:
+            expected = bool
+        elif self._type is DataType.NUMBER:
+            expected = PY_NUMBER
+        elif self._type is DataType.STRING:
+            expected = str
+        else:
+            raise ValueError(f'unexpected type {self._type}')
+        for value in values:
+            if not isinstance(value, expected):
+                raise TypeError(f'{value!r} is not of type {expected}')
+
+    @property
+    def type(self) -> DataType:
+        return self._type
+
+    @classmethod
+    def booleans(cls, name: str = 'bool') -> 'EnumeratedType':
+        return cls(name, type=DataType.BOOL, values=(False, True))
+
+
+@frozen
+class NumericType(TypeToken):
+    min_value: Union[int, float, complex] = field(default=-INF, validator=instance_of(PY_NUMBER))
+    max_value: Union[int, float, complex] = field(default=INF, validator=instance_of(PY_NUMBER))
+
+    @max_value.validator
+    def _check_max_value(self, _attribute, value: Union[int, float]):
+        if value < self.min_value:
+            raise ValueError(f'max_value={value} < min_value={self.min_value}')
+
+    @property
+    def type(self) -> DataType:
+        return DataType.NUMBER
+
+    @classmethod
+    def uint8(cls, name: str = 'uint8') -> 'NumericType':
+        min_value = 0
+        max_value = 255
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def uint16(cls, name: str = 'uint16') -> 'NumericType':
+        min_value = 0
+        max_value = 65535
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def uint32(cls, name: str = 'uint32') -> 'NumericType':
+        min_value = 0
+        max_value = 4294967295
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def uint64(cls, name: str = 'uint64') -> 'NumericType':
+        min_value = 0
+        max_value = 18446744073709551615
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def int8(cls, name: str = 'int8') -> 'NumericType':
+        min_value = -128
+        max_value = 127
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def int16(cls, name: str = 'int16') -> 'NumericType':
+        min_value = -32768
+        max_value = 32767
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def int32(cls, name: str = 'int32') -> 'NumericType':
+        min_value = -2147483648
+        max_value = 2147483647
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def int64(cls, name: str = 'int64') -> 'NumericType':
+        min_value = -9223372036854775808
+        max_value = 9223372036854775807
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def float32(cls, name: str = 'float32') -> 'NumericType':
+        min_value = -3.3999999521443642E+38
+        max_value = 3.3999999521443642E+38
+        return cls(name, min_value=min_value, max_value=max_value)
+
+    @classmethod
+    def float64(cls, name: str = 'float64') -> 'NumericType':
+        min_value = -1.7E+308
+        max_value = 1.7E+308
+        return cls(name, min_value=min_value, max_value=max_value)
+
+
+@frozen
+class MessageType(TypeToken):
+    fields: Mapping[str, TypeToken] = field(factory=dict)
+    constants: Mapping[str, Any] = field(factory=dict)
+
+    @property
+    def type(self) -> DataType:
+        return DataType.MESSAGE
+
+    def leaf_fields(self) -> Mapping[str, TypeToken]:
+        fields = {}
+        for name, token in self.fields.items():
+            if token.is_message:
+                for subname, subtoken in token.leaf_fields():
+                    fields[f'{name}.{subname}'] = subtoken
+            else:
+                fields[name] = token
+        return fields
+
+
+@frozen
+class ArrayType(TypeToken):
+    subtype: TypeToken
+    length: int = field(default=-1, validator=ge(-1))
+
+    @property
+    def type(self) -> DataType:
+        return DataType.ARRAY
+
+    @property
+    def is_fixed_length(self) -> bool:
+        return self.length >= 0
+
+
+###############################################################################
+# Exported Constants
+###############################################################################
+
+BOOLEANS: Final[EnumeratedType] = EnumeratedType.booleans()
+UINT8: Final[NumericType] = NumericType.uint8()
+UINT16: Final[NumericType] = NumericType.uint16()
+UINT32: Final[NumericType] = NumericType.uint32()
+UINT64: Final[NumericType] = NumericType.uint64()
+INT8: Final[NumericType] = NumericType.int8()
+INT16: Final[NumericType] = NumericType.int16()
+INT32: Final[NumericType] = NumericType.int32()
+INT64: Final[NumericType] = NumericType.int64()
+FLOAT32: Final[NumericType] = NumericType.float32()
+FLOAT64: Final[NumericType] = NumericType.float64()
+STRINGS: Final[StringType] = StringType('string')
+
