a830e907be645cc5bba44beac8de37eeb41d0fa7
André Santos <git-afsantos@users.noreply.github.com>
2023-08-14 10:47:18 +0100

    WIP

--- src/hpl/ast/properties.py
+++ src/hpl/ast/properties.py
@@ -5,14 +5,17 @@
 # Imports
 ###############################################################################
 
-from typing import Final, Iterable, Tuple
+from enum import Enum, auto
+from typing import Final, Iterable, Optional, Tuple
 
-import enum
 from math import isclose
 
 from attrs import field, frozen
+from attrs.validators import instance_of, in_, optional
 
-from hpl.errors import HplSanityError, HplTypeError
+from hpl.ast.base import HplAstObject
+from hpl.ast.events import HplEvent
+from hpl.errors import HplSanityError, expected_not_none, invalid_attr
 
 ###############################################################################
 # Constants
@@ -20,200 +23,70 @@ from hpl.errors import HplSanityError, HplTypeError
 
 INF: Final[float] = float('inf')
 
+###############################################################################
+# Scopes
+###############################################################################
 
 
+class ScopeType(Enum):
+    GLOBAL = auto()
+    AFTER_UNTIL = auto()
+    AFTER = auto()
+    UNTIL = auto()
+
+    @property
+    def is_after(self) -> bool:
+        return self is ScopeType.AFTER or self is ScopeType.AFTER_UNTIL
+
+    @property
+    def is_until(self) -> bool:
+        return self is ScopeType.UNTIL or self is ScopeType.AFTER_UNTIL
+
+    @property
+    def is_global(self) -> bool:
+        return self is ScopeType.GLOBAL
+
+    @property
+    def has_activator(self) -> bool:
+        return self.is_after
+
+    @property
+    def has_terminator(self) -> bool:
+        return self.is_until
 
 
 @frozen
-class HplSpecification(HplAstObject):
-    properties: Tuple['HplProperty']
-
-    @property
-    def is_specification(self):
-        return True
-
-    def children(self):
-        return self.properties
-
-    def sanity_check(self):
-        for prop in self.properties:
-            prop.sanity_check()
-
-    def clone(self):
-        return HplSpecification([p.clone() for p in self.properties])
-
-    def __eq__(self, other):
-        if not isinstance(other, HplSpecification):
-            return False
-        return set(self.properties) == set(other.properties)
-
-    def __hash__(self):
-        return hash(set(self.properties))
-
-    def __str__(self):
-        return "\n".join(str(prop) for prop in self.properties)
-
-    def __repr__(self):
-        return "{}({})".format(type(self).__name__, repr(self.properties))
-
-
-class HplProperty(HplAstObject):
-    __slots__ = ("scope", "pattern", "metadata")
-
-    def __init__(self, scope, pattern, meta=None):
-        self.scope = scope # HplScope
-        self.pattern = pattern # HplPattern
-        self.metadata = meta if meta is not None else {}
-
-    @property
-    def is_property(self):
-        return True
-
-    @property
-    def is_safety(self):
-        return self.pattern.is_safety
-
-    @property
-    def is_liveness(self):
-        return self.pattern.is_liveness
-
-    @property
-    def uid(self):
-        return self.metadata.get("id", None)
-
-    def children(self):
-        return (self.scope, self.pattern)
-
-    def is_fully_typed(self):
-        for event in self.events():
-            for e in event.simple_events():
-                if not e.predicate.is_fully_typed():
-                    return False
-        return True
-
-    def refine_types(self, rostypes, aliases=None):
-        # rostypes: string (topic) -> ROS Type Token
-        # aliases:  string (alias) -> ROS Type Token
-        for event in self.events():
-            for e in event.simple_events():
-                rostype = rostypes.get(e.topic)
-                if rostype is None:
-                    raise HplTypeError.undefined(e.topic)
-                e.refine_types(rostype, aliases=aliases)
-
-    def events(self):
-        if self.scope.activator is not None:
-            yield self.scope.activator
-        yield self.pattern.behaviour
-        if self.pattern.trigger is not None:
-            yield self.pattern.trigger
-        if self.scope.terminator is not None:
-            yield self.scope.terminator
-
-    def sanity_check(self):
-        initial = self._check_activator()
-        if self.pattern.is_absence or self.pattern.is_existence:
-            self._check_behaviour(initial)
-        elif self.pattern.is_requirement:
-            aliases = self._check_behaviour(initial)
-            self._check_trigger(aliases)
-        elif self.pattern.is_response or self.pattern.is_prevention:
-            aliases = self._check_trigger(initial)
-            self._check_behaviour(aliases)
-        else:
-            assert False, 'unexpected pattern type: ' + repr(self.pattern.pattern_type)
-        self._check_terminator(initial)
-
-    def clone(self):
-        return HplProperty(self.scope.clone(), self.pattern.clone(),
-                           meta=dict(self.metadata))
-
-    def _check_activator(self):
-        p = self.scope.activator
-        if p is not None:
-            refs = p.external_references()
-            if refs:
-                raise HplSanityError(
-                    "references to undefined events: " + repr(refs))
-            return p.aliases()
-        return ()
-
-    def _check_trigger(self, available):
-        a = self.pattern.trigger
-        assert a is not None
-        self._check_refs_defined(a.external_references(), available)
-        aliases = a.aliases()
-        self._check_duplicates(aliases, available)
-        return aliases + available
-
-    def _check_behaviour(self, available):
-        b = self.pattern.behaviour
-        self._check_refs_defined(b.external_references(), available)
-        aliases = b.aliases()
-        self._check_duplicates(aliases, available)
-        return aliases + available
-
-    def _check_terminator(self, available):
-        q = self.scope.terminator
-        if q is not None:
-            self._check_refs_defined(q.external_references(), available)
-            self._check_duplicates(q.aliases(), available)
-
-    def _check_refs_defined(self, refs, available):
-        for ref in refs:
-            if not ref in available:
-                raise HplSanityError(
-                    "reference to undefined event: " + repr(ref))
-
-    def _check_duplicates(self, aliases, available):
-        for alias in aliases:
-            if alias in available:
-                raise HplSanityError("duplicate alias: " + repr(alias))
-
-    def __eq__(self, other):
-        if not isinstance(other, HplProperty):
-            return False
-        return self.pattern == other.pattern and self.scope == other.scope
-
-    def __hash__(self):
-        return 31 * hash(self.scope) + hash(self.pattern)
-
-    def __str__(self):
-        return "{}: {}".format(self.scope, self.pattern)
-
-    def __repr__(self):
-        return "{}({}, {})".format(type(self).__name__,
-            repr(self.scope), repr(self.pattern))
-
-
 class HplScope(HplAstObject):
-    __slots__ = ("scope_type", "activator", "terminator")
+    scope_type: ScopeType = field(validator=in_(ScopeType))
+    activator: Optional[HplEvent] = field(
+        default=None,
+        validator=optional(instance_of(HplEvent)),
+    )
+    terminator: Optional[HplEvent] = field(
+        default=None,
+        validator=optional(instance_of(HplEvent)),
+    )
 
-    GLOBAL = 1
-    AFTER_UNTIL = 2
-    AFTER = 3
-    UNTIL = 4
+    @activator.validator
+    def _check_activator(self, attribute, event: Optional[HplEvent]):
+        if self.scope_type.has_activator:
+            if event is None:
+                raise expected_not_none(attribute.name, self)
+        else:
+            if event is not None:
+                raise invalid_attr(attribute.name, None, event, self)
 
-    def __init__(self, scope, activator=None, terminator=None):
-        if scope == self.GLOBAL:
-            if activator is not None:
-                raise ValueError(activator)
-            if terminator is not None:
-                raise ValueError(terminator)
-        elif scope == self.AFTER:
-            if terminator is not None:
-                raise ValueError(terminator)
-        elif scope == self.UNTIL:
-            if activator is not None:
-                raise ValueError(activator)
-        elif scope != self.AFTER_UNTIL:
-            raise ValueError(scope)
-        self.scope_type = scope
-        self.activator = activator # HplEvent | None
-        self.terminator = terminator # HplEvent | None
+    @terminator.validator
+    def _check_terminator(self, attribute, event: Optional[HplEvent]):
+        if self.scope_type.has_terminator:
+            if event is None:
+                raise expected_not_none(attribute.name, self)
+        else:
+            if event is not None:
+                raise invalid_attr(attribute.name, None, event, self)
 
     @property
-    def is_scope(self):
+    def is_scope(self) -> bool:
         return True
 
     @classmethod
@@ -291,6 +164,12 @@ class HplScope(HplAstObject):
             repr(self.activator), repr(self.terminator))
 
 
+###############################################################################
+# Patterns
+###############################################################################
+
+
+@frozen
 class HplPattern(HplAstObject):
     __slots__ = ("pattern_type", "behaviour", "trigger", "min_time", "max_time")
 
@@ -429,3 +308,138 @@ class HplPattern(HplAstObject):
         return "{}({}, {}, {}, min_time={}, max_time={})".format(
             type(self).__name__, repr(self.pattern_type), repr(self.behaviour),
             repr(self.trigger), repr(self.min_time), repr(self.max_time))
+
+
+###############################################################################
+# Properties
+###############################################################################
+
+
+@frozen
+class HplProperty(HplAstObject):
+    __slots__ = ("scope", "pattern", "metadata")
+
+    def __init__(self, scope, pattern, meta=None):
+        self.scope = scope # HplScope
+        self.pattern = pattern # HplPattern
+        self.metadata = meta if meta is not None else {}
+
+    @property
+    def is_property(self):
+        return True
+
+    @property
+    def is_safety(self):
+        return self.pattern.is_safety
+
+    @property
+    def is_liveness(self):
+        return self.pattern.is_liveness
+
+    @property
+    def uid(self):
+        return self.metadata.get("id", None)
+
+    def children(self):
+        return (self.scope, self.pattern)
+
+    def is_fully_typed(self):
+        for event in self.events():
+            for e in event.simple_events():
+                if not e.predicate.is_fully_typed():
+                    return False
+        return True
+
+    def refine_types(self, rostypes, aliases=None):
+        # rostypes: string (topic) -> ROS Type Token
+        # aliases:  string (alias) -> ROS Type Token
+        for event in self.events():
+            for e in event.simple_events():
+                rostype = rostypes.get(e.topic)
+                if rostype is None:
+                    raise HplTypeError.undefined(e.topic)
+                e.refine_types(rostype, aliases=aliases)
+
+    def events(self):
+        if self.scope.activator is not None:
+            yield self.scope.activator
+        yield self.pattern.behaviour
+        if self.pattern.trigger is not None:
+            yield self.pattern.trigger
+        if self.scope.terminator is not None:
+            yield self.scope.terminator
+
+    def sanity_check(self):
+        initial = self._check_activator()
+        if self.pattern.is_absence or self.pattern.is_existence:
+            self._check_behaviour(initial)
+        elif self.pattern.is_requirement:
+            aliases = self._check_behaviour(initial)
+            self._check_trigger(aliases)
+        elif self.pattern.is_response or self.pattern.is_prevention:
+            aliases = self._check_trigger(initial)
+            self._check_behaviour(aliases)
+        else:
+            assert False, 'unexpected pattern type: ' + repr(self.pattern.pattern_type)
+        self._check_terminator(initial)
+
+    def clone(self):
+        return HplProperty(self.scope.clone(), self.pattern.clone(),
+                           meta=dict(self.metadata))
+
+    def _check_activator(self):
+        p = self.scope.activator
+        if p is not None:
+            refs = p.external_references()
+            if refs:
+                raise HplSanityError(
+                    "references to undefined events: " + repr(refs))
+            return p.aliases()
+        return ()
+
+    def _check_trigger(self, available):
+        a = self.pattern.trigger
+        assert a is not None
+        self._check_refs_defined(a.external_references(), available)
+        aliases = a.aliases()
+        self._check_duplicates(aliases, available)
+        return aliases + available
+
+    def _check_behaviour(self, available):
+        b = self.pattern.behaviour
+        self._check_refs_defined(b.external_references(), available)
+        aliases = b.aliases()
+        self._check_duplicates(aliases, available)
+        return aliases + available
+
+    def _check_terminator(self, available):
+        q = self.scope.terminator
+        if q is not None:
+            self._check_refs_defined(q.external_references(), available)
+            self._check_duplicates(q.aliases(), available)
+
+    def _check_refs_defined(self, refs, available):
+        for ref in refs:
+            if not ref in available:
+                raise HplSanityError(
+                    "reference to undefined event: " + repr(ref))
+
+    def _check_duplicates(self, aliases, available):
+        for alias in aliases:
+            if alias in available:
+                raise HplSanityError("duplicate alias: " + repr(alias))
+
+    def __eq__(self, other):
+        if not isinstance(other, HplProperty):
+            return False
+        return self.pattern == other.pattern and self.scope == other.scope
+
+    def __hash__(self):
+        return 31 * hash(self.scope) + hash(self.pattern)
+
+    def __str__(self):
+        return "{}: {}".format(self.scope, self.pattern)
+
+    def __repr__(self):
+        return "{}({}, {})".format(type(self).__name__,
+            repr(self.scope), repr(self.pattern))
